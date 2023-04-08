cfb4447592b859eb1a779936eec7fb07a6dc9b3a
Jared Hollingsworth <123146317+jared2468@users.noreply.github.com>
2023-04-07 23:27:39 -0700

    first commit

---
+++ Week10Coding.js
@@ -0,0 +1,63 @@
+// console.log('Week10'); // used to test that my Javascript was connected to my HTML index
+// let myInsInventoryForm = document.createElement('form');
+
+
+// let myInsTypeLabel = document.createElement('label');
+// let myInsTypeInput = document.createElement('input');
+
+// let myInsMakeLabel = document.createElement('label');
+// let myInsMakeInput = document.createElement('input');
+
+// let myInsModelLabel = document.createElement('label');
+// let myInsModelInput = document.createElement('input');
+
+// let myInsColorLabel = document.createElement('label');
+// let myInsColorInput = document.createElement('input');
+
+// myInsTypeLabel.innerHTML = 'Type';
+// myInsMakeLabel.innerHTML = 'Make';
+// myInsModelLabel.innerHTML = 'Model';
+// myInsColorLabel.innerHTML = 'Color';
+
+
+// myInsInventoryForm.append(myInsTypeLabel);
+// myInsInventoryForm.append(myInsTypeInput);
+// myInsInventoryForm.append(myInsMakeLabel);
+// myInsInventoryForm.append(myInsMakeInput);
+
+// myInsInventoryForm.append(myInsModelLabel);
+// myInsInventoryForm.append(myInsModelInput);
+// myInsInventoryForm.append(myInsColorLabel);
+// myInsInventoryForm.append(myInsColorInput);
+// document.body.append(myInsInventoryForm);
+
+// mySubmitButton = document.createElement('button');
+// mySubmitButton.innerHTML = 'Submit';
+// myInsInventoryForm.append(mySubmitButton);
+
+// let myInventoryTable = document.createElement('table');
+// let myInventoryTr = document.createElement('tr');
+// let myTdType = document.createElement('tr');
+// let myTdMake = document.createElement('tr');
+// let myTdModel = document.createElement('tr');
+// let myTdColor = document.createElement('tr');
+// myTdType.innerHTML = 'Type';
+// myTdMake.innerHTML = 'Make';
+// myTdModel.innerHTML = 'Model';
+// myTdColor.innerHTML = 'Color'
+
+// myInventoryTable.append(myInventoryTr);
+// myInventoryTr.append(myTdType);
+// myInventoryTr.append(myTdMake);
+// myInventoryTr.append(myTdModel);
+// myInventoryTr.append(myTdColor);
+
+// document.body.append(myInventoryTable);
+
+
+
+
+
+// console.log(myForm);
+// console.log(myNameLabel);
+// console.log(myNameInput);
